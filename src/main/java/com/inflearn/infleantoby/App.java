26ce078c040e96cf5d40b60d39baf18864bf9c40
kuroneko <drivespublic@gmail.com>
2023-08-11 20:08:03 +0900

    TestRestTemplate 사용.

--- src/main/java/com/inflearn/infleantoby/App.java
+++ src/main/java/com/inflearn/infleantoby/App.java
@@ -1,6 +1,8 @@
 package com.inflearn.infleantoby;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,10 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan(basePackages = "com.inflearn.*")
 public class App {
-
-
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
 
     @Bean
     public DispatcherServlet dispatcherServlet() {
---
+++ src/test/java/com/inflearn/infleantoby/AppTest.java
@@ -0,0 +1,31 @@
+package com.inflearn.infleantoby;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void helloTest() {
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        String nameParameter = "dong";
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8080/hello?name={name}", String.class, nameParameter);
+
+        assertEquals(responseEntity.getStatusCode().value(), 200);
+        assertEquals(responseEntity.getBody(), "hello " + nameParameter);
+        assertTrue(Objects.requireNonNull(responseEntity.getHeaders().getContentType()).toString().startsWith(MediaType.TEXT_PLAIN_VALUE));
+
+
+    }
+
+}
